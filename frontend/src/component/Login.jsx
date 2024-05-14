3c3d052625ff33df67ddfc8b461cd83b9838660e
saloni singh <salonisingh1322004@gmail.com>
2024-05-13 22:29:14 -0700

    signup

--- frontend/src/component/Login.jsx
+++ frontend/src/component/Login.jsx
@@ -3,85 +3,92 @@ import React from 'react'
 const Login = () => {
   return (
     <div>
-      <section className="vh-100 gradient-custom">
-  <div className="container py-5 h-100">
-    <div className="row d-flex justify-content-center align-items-center h-100">
-      <div className="col-12 col-md-8 col-lg-6 col-xl-5">
-        <div
-          className="card bg-dark text-white"
-          style={{ borderRadius: "1rem" }}
-        >
-          <div className="card-body p-5 text-center">
-            <div className="mb-md-5 mt-md-4 pb-5">
-              <h2 className="fw-bold mb-2 text-uppercase">Login</h2>
-              <p className="text-white-50 mb-5">
-                Please enter your login and password!
-              </p>
-              <div
-                data-mdb-input-init=""
-                className="form-outline form-white mb-4"
-              >
+      <>
+  {/* Section: Design Block */}
+  <section className=" text-center text-lg-start">
+    <style
+      dangerouslySetInnerHTML={{
+        __html:
+          "\n    .rounded-t-5 {\n      border-top-left-radius: 0.5rem;\n      border-top-right-radius: 0.5rem;\n    }\n\n    @media (min-width: 992px) {\n      .rounded-tr-lg-0 {\n        border-top-right-radius: 0;\n      }\n\n      .rounded-bl-lg-5 {\n        border-bottom-left-radius: 0.5rem;\n      }\n    }\n  "
+      }}
+    />
+    <div className="card mb-3">
+      <div className="row g-0 d-flex align-items-center">
+        <div className="col-lg-4 d-none d-lg-flex">
+          <img
+            src="https://mdbootstrap.com/img/new/ecommerce/vertical/004.jpg"
+            alt="Trendy Pants and Shoes"
+            className="w-100 rounded-t-5 rounded-tr-lg-0 rounded-bl-lg-5"
+          />
+        </div>
+        <div className="col-lg-8">
+          <div className="card-body py-5 px-md-5">
+            <form>
+              {/* Email input */}
+              <div data-mdb-input-init="" className="form-outline mb-4">
                 <input
                   type="email"
-                  id="typeEmailX"
-                  className="form-control form-control-lg"
+                  id="form2Example1"
+                  className="form-control"
                 />
-                <label className="form-label" htmlFor="typeEmailX">
-                  Email
+                <label className="form-label" htmlFor="form2Example1">
+                  Email address
                 </label>
               </div>
-              <div
-                data-mdb-input-init=""
-                className="form-outline form-white mb-4"
-              >
+              {/* Password input */}
+              <div data-mdb-input-init="" className="form-outline mb-4">
                 <input
                   type="password"
-                  id="typePasswordX"
-                  className="form-control form-control-lg"
+                  id="form2Example2"
+                  className="form-control"
                 />
-                <label className="form-label" htmlFor="typePasswordX">
+                <label className="form-label" htmlFor="form2Example2">
                   Password
                 </label>
               </div>
-              <p className="small mb-5 pb-lg-2">
-                <a className="text-white-50" href="#!">
-                  Forgot password?
-                </a>
-              </p>
+              {/* 2 column grid layout for inline styling */}
+              <div className="row mb-4">
+                <div className="col d-flex justify-content-center">
+                  {/* Checkbox */}
+                  <div className="form-check">
+                    <input
+                      className="form-check-input"
+                      type="checkbox"
+                      defaultValue=""
+                      id="form2Example31"
+                      defaultChecked=""
+                    />
+                    <label
+                      className="form-check-label"
+                      htmlFor="form2Example31"
+                    >
+                      {" "}
+                      Remember me{" "}
+                    </label>
+                  </div>
+                </div>
+                <div className="col">
+                  {/* Simple link */}
+                  <a href="#!">Forgot password?</a>
+                </div>
+              </div>
+              {/* Submit button */}
               <button
+                type="button"
                 data-mdb-button-init=""
                 data-mdb-ripple-init=""
-                className="btn btn-outline-light btn-lg px-5"
-                type="submit"
+                className="btn btn-primary btn-block mb-4"
               >
-                Login
+                Sign in
               </button>
-              <div className="d-flex justify-content-center text-center mt-4 pt-1">
-                <a href="#!" className="text-white">
-                  <i className="fab fa-facebook-f fa-lg" />
-                </a>
-                <a href="#!" className="text-white">
-                  <i className="fab fa-twitter fa-lg mx-4 px-2" />
-                </a>
-                <a href="#!" className="text-white">
-                  <i className="fab fa-google fa-lg" />
-                </a>
-              </div>
-            </div>
-            <div>
-              <p className="mb-0">
-                Don't have an account?{" "}
-                <a href="#!" className="text-white-50 fw-bold">
-                  Sign Up
-                </a>
-              </p>
-            </div>
+            </form>
           </div>
         </div>
       </div>
     </div>
-  </div>
-</section>
+  </section>
+  {/* Section: Design Block */}
+</>
 
     </div>
   )
--- frontend/src/component/Signup.jsx
+++ frontend/src/component/Signup.jsx
@@ -1,213 +1,103 @@
 import React from 'react'
-import {useFormik} from 'formik'
-import * as Yup from 'yup'
- 
-
-const SignupSchema = Yup.object().shape({
-  name:Yup.string()
-  .required('Name is required')
-  .min(3, 'name must be atleast  3 characters')
-  .max(15, 'name must be at most  15 characters'),
-  email : Yup.string()
-  .email('Invalid Email ')
-  .required("Email is required"),
-  password : Yup.string()
-  .required("Password is required")
-  .min(8, 'Password must be at least 8 characters')
-  .max(15, 'Password must not exceed 15 characters')
-})
-
 
 const Signup = () => {
-   // step1 : formik initialization
-   const SignupForm = useFormik({
-    initialValues: {
-        name:'',
-        email:'',
-        password:'',
-    },
-
-    onSubmit: async(values,action) =>{
-        console.log(values);
-        const res = await fetch('http://localhost:3000/user/add',{
-            method:"POST",
-            body: JSON.stringify(values),
-            headers:{
-                "Content-Type":"application/json"
-            }
-        });
-        console.log(res.status)
-        action.resetForm()
-
-        if (res.status === 200){
-            enqueueSnackbar('Signup successful', { variant: 'success' })
-            }else{
-                enqueueSnackbar('Signup failed', { variant: 'error'})
-        }
-    },
-        
-
-    validationSchema: SignupSchema
-   })
-   
   return (
     <div>
       <>
   {/* Section: Design Block */}
-  <section className="background-radial-gradient overflow-hidden">
-    <style
-      dangerouslySetInnerHTML={{
-        __html:
-          "\n    .background-radial-gradient {\n      background-color: hsl(218, 41%, 15%);\n      background-image: radial-gradient(650px circle at 0% 0%,\n          hsl(218, 41%, 35%) 15%,\n          hsl(218, 41%, 30%) 35%,\n          hsl(218, 41%, 20%) 75%,\n          hsl(218, 41%, 19%) 80%,\n          transparent 100%),\n        radial-gradient(1250px circle at 100% 100%,\n          hsl(218, 41%, 45%) 15%,\n          hsl(218, 41%, 30%) 35%,\n          hsl(218, 41%, 20%) 75%,\n          hsl(218, 41%, 19%) 80%,\n          transparent 100%);\n    }\n\n    #radius-shape-1 {\n      height: 220px;\n      width: 220px;\n      top: -60px;\n      left: -130px;\n      background: radial-gradient(#44006b, #ad1fff);\n      overflow: hidden;\n    }\n\n    #radius-shape-2 {\n      border-radius: 38% 62% 63% 37% / 70% 33% 67% 30%;\n      bottom: -60px;\n      right: -110px;\n      width: 300px;\n      height: 300px;\n      background: radial-gradient(#44006b, #ad1fff);\n      overflow: hidden;\n    }\n\n    .bg-glass {\n      background-color: hsla(0, 0%, 100%, 0.9) !important;\n      backdrop-filter: saturate(200%) blur(25px);\n    }\n  "
-      }}
-    />
-    <div className="container px-4 py-5 px-md-5 text-center text-lg-start my-5">
-      <div className="row gx-lg-5 align-items-center mb-5">
-        <div className="col-lg-6 mb-5 mb-lg-0" style={{ zIndex: 10 }}>
-          <h1
-            className="my-5 display-5 fw-bold ls-tight"
-            style={{ color: "hsl(218, 81%, 95%)" }}
-          >
-            The best offer <br />
-            <span style={{ color: "hsl(218, 81%, 75%)" }}>
-              for your business
-            </span>
-          </h1>
-          <p
-            className="mb-4 opacity-70"
-            style={{ color: "hsl(218, 81%, 85%)" }}
-          >
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-            Temporibus, expedita iusto veniam atque, magni tempora mollitia
-            dolorum consequatur nulla, neque debitis eos reprehenderit quasi ab
-            ipsum nisi dolorem modi. Quos?
-          </p>
-        </div>
-        <div className="col-lg-6 mb-5 mb-lg-0 position-relative">
-          <div
-            id="radius-shape-1"
-            className="position-absolute rounded-circle shadow-5-strong"
-          />
-          <div
-            id="radius-shape-2"
-            className="position-absolute shadow-5-strong"
-          />
-          <div className="card bg-glass">
-            <div className="card-body px-4 py-5 px-md-5">
-              <form>
-                {/* 2 column grid layout with text inputs for the first and last names */}
-                <div className="row">
-                  <div className="col-md-6 mb-4">
-                    <div data-mdb-input-init="" className="form-outline">
-                      <input
-                        type="text"
-                        id="form3Example1"
-                        className="form-control"
-                      />
-                      <label className="form-label" htmlFor="form3Example1">
-                        First name
-                      </label>
+  <section className="">
+    {/* Jumbotron */}
+    <div
+      className="px-4 py-5 px-md-5 text-center text-lg-start"
+      style={{ backgroundColor: "hsl(0, 0%, 96%)" }}
+    >
+      <div className="container">
+        <div className="row gx-lg-5 align-items-center">
+          <div className="col-lg-6 mb-5 mb-lg-0">
+            <h1 className="my-5 display-3 fw-bold ls-tight">
+              The best offer <br />
+              <span className="text-primary">for your business</span>
+            </h1>
+            <p style={{ color: "hsl(217, 10%, 50.8%)" }}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet,
+              itaque accusantium odio, soluta, corrupti aliquam quibusdam
+              tempora at cupiditate quis eum maiores libero veritatis? Dicta
+              facilis sint aliquid ipsum atque?
+            </p>
+          </div>
+          <div className="col-lg-6 mb-5 mb-lg-0">
+            <div className="card">
+              <div className="card-body py-5 px-md-5">
+                <form>
+                  {/* 2 column grid layout with text inputs for the first and last names */}
+                  <div className="row">
+                    <div className="col-md-6 mb-4">
+                      <div data-mdb-input-init="" className="form-outline">
+                        <input
+                          type="text"
+                          id="form3Example1"
+                          className="form-control"
+                        />
+                        <label className="form-label" htmlFor="form3Example1">
+                          First name
+                        </label>
+                      </div>
+                    </div>
+                    <div className="col-md-6 mb-4">
+                      <div data-mdb-input-init="" className="form-outline">
+                        <input
+                          type="text"
+                          id="form3Example2"
+                          className="form-control"
+                        />
+                        <label className="form-label" htmlFor="form3Example2">
+                          Last name
+                        </label>
+                      </div>
                     </div>
                   </div>
-                  <div className="col-md-6 mb-4">
-                    <div data-mdb-input-init="" className="form-outline">
-                      <input
-                        type="text"
-                        id="form3Example2"
-                        className="form-control"
-                      />
-                      <label className="form-label" htmlFor="form3Example2">
-                        Last name
-                      </label>
-                    </div>
+                  {/* Email input */}
+                  <div data-mdb-input-init="" className="form-outline mb-4">
+                    <input
+                      type="email"
+                      id="form3Example3"
+                      className="form-control"
+                    />
+                    <label className="form-label" htmlFor="form3Example3">
+                      Email address
+                    </label>
                   </div>
-                </div>
-                {/* Email input */}
-                <div data-mdb-input-init="" className="form-outline mb-4">
-                  <input
-                    type="email"
-                    id="form3Example3"
-                    className="form-control"
-                  />
-                  <label className="form-label" htmlFor="form3Example3">
-                    Email address
-                  </label>
-                </div>
-                {/* Password input */}
-                <div data-mdb-input-init="" className="form-outline mb-4">
-                  <input
-                    type="password"
-                    id="form3Example4"
-                    className="form-control"
-                  />
-                  <label className="form-label" htmlFor="form3Example4">
-                    Password
-                  </label>
-                </div>
-                {/* Checkbox */}
-                <div className="form-check d-flex justify-content-center mb-4">
-                  <input
-                    className="form-check-input me-2"
-                    type="checkbox"
-                    defaultValue=""
-                    id="form2Example33"
-                    defaultChecked=""
-                  />
-                  <label className="form-check-label" htmlFor="form2Example33">
-                    Subscribe to our newsletter
-                  </label>
-                </div>
-                {/* Submit button */}
-                <button
-                  type="submit"
-                  data-mdb-button-init=""
-                  data-mdb-ripple-init=""
-                  className="btn btn-primary btn-block mb-4"
-                >
-                  Sign up
-                </button>
-                {/* Register buttons */}
-                <div className="text-center">
-                  <p>or sign up with:</p>
+                  {/* Password input */}
+                  <div data-mdb-input-init="" className="form-outline mb-4">
+                    <input
+                      type="password"
+                      id="form3Example4"
+                      className="form-control"
+                    />
+                    <label className="form-label" htmlFor="form3Example4">
+                      Password
+                    </label>
+                  </div>
+                  
+                  {/* Submit button */}
                   <button
-                    type="button"
+                    type="submit"
                     data-mdb-button-init=""
                     data-mdb-ripple-init=""
-                    className="btn btn-link btn-floating mx-1"
+                    className="btn btn-primary btn-block mb-4"
                   >
-                    <i className="fab fa-facebook-f" />
+                    Sign up
                   </button>
-                  <button
-                    type="button"
-                    data-mdb-button-init=""
-                    data-mdb-ripple-init=""
-                    className="btn btn-link btn-floating mx-1"
-                  >
-                    <i className="fab fa-google" />
-                  </button>
-                  <button
-                    type="button"
-                    data-mdb-button-init=""
-                    data-mdb-ripple-init=""
-                    className="btn btn-link btn-floating mx-1"
-                  >
-                    <i className="fab fa-twitter" />
-                  </button>
-                  <button
-                    type="button"
-                    data-mdb-button-init=""
-                    data-mdb-ripple-init=""
-                    className="btn btn-link btn-floating mx-1"
-                  >
-                    <i className="fab fa-github" />
-                  </button>
-                </div>
-              </form>
+                  
+                  
+                </form>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
+    {/* Jumbotron */}
   </section>
   {/* Section: Design Block */}
 </>
