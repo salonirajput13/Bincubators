0e7dbef2bf619c5a7aa482921abb2b664fcfdd01
Areeba Shakeel <shakeelareeba02@gmail.com>
2024-05-21 10:24:02 +0530

    ...

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,49 +1,89 @@
 import React from 'react'
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
-import {SnackbarProvider} from 'notistack'
-import { AppProvider } from './AppContext'
-import Navbar from './component/Navbar'
-import Home from './component/Home'
-import Login from './component/Login'
-import Signup from './component/Signup'
-import Contactus from './component/Contactus'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Navbar from './components/navbar'
+import Home from './components/Home'
 import './App.css'
-import Services from './component/services'
-import Base from './component/Admin/Base'
-import Index from './component/Admin/Index'
-import Header from './component/Admin/Header'
-import Sidebar from './component/Admin/Sidebar'
-// import AddServices from './component/AddServices'
- 
+import Services from './components/services'
+import Login from './components/Login'
+import Signup from './components/Signup'
+// import About from './components/About'
+import PlanningServices from './components/PlanningServices'
+import LaunchingServices from './components/LaunchingServices'
+import ManagingServices from './components/ManagingServices'
+import AddServices from './components/AddServices'
+import { AppProvider } from '../AppContext'
+import { SnackbarProvider } from 'notistack'
+import Roadmap from './components/Roadmap'
+import UserAuth from './UserAuth';
+import AdminAuth from './AdminAuth'
+import Pmanage from './components/Pmanage'
+import Lmanage from './components/Lmanage'
+import Mmanage from './components/Mmanage'
+import AdminProfile from './components/AdminProfile'
+import Dashboard from './components/Dashboard'
+import ViewService from './components/ViewService'
+import Contact2 from './components/Contact2'
+import Admin from './components/Admin/Index'
+import Header from './components/Admin/Header'
+import Main from './components/Admin/Index';
+import Sidebar from './components/Admin/Sidebar'
+import Base from './components/Admin/Base'
+import Umanage from './components/Umanage'
+import UpdateService from './components/UpdateService'
+import EnquiryData from './components/EnquiryData'
+import UserProfile from './components/UserProfile'
+import ForgetPassword from './components/ForgetPassword'
+// import Chat from './components/Chat'
+// import ExpertChat from './components/ExpertCaht'
+// import UserChat from './components/UserChat'
+
 
 const App = () => {
   return (
     <div>
       <SnackbarProvider>
         <BrowserRouter>
-        <AppProvider>
-        <Navbar/>
-        <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/Home'element={<Home/>}/>
-          <Route path='/Signup' element={<Signup/>}/>
-          <Route path='/Login' element={<Login/>}/>
-          <Route path='/Contactus' element={<Contactus/>}/>
-          <Route path='/services' element={<Services/>}/>
-          <Route path='/AddServices' element={<AddServices/>}/>
-          <Route path='/LaunchingServices' element={<LaunchingServices/>}/>
-          <Route path='/Lmanage' element={<Lmanage/>}/>
-          <Route path='/ManagingServices' element={<ManagingServices/>}/>
-          
-          <Route path='/Admin' element={<Admin />}>
-          <Route path='Base'element={<Base/>}/>
-          <Route path='Header'element={<Header/>}/>
-          <Route path='Index'element={<Index/>}/>
-          <Route path='Sidebar'element={<Sidebar/>}/>
-          </Route>
+          <AppProvider>
+            <Navbar />
 
-        </Routes>
-        </AppProvider>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/services' element={<Services />} />
+              <Route path='/Signup' element={<Signup />} />
+              <Route path='/Login' element={<Login />} />
+
+              <Route path='/PlanningServices' element={<PlanningServices />} />
+              <Route path='/LaunchingServices' element={<LaunchingServices />} />
+              <Route path='/ManagingServices' element={<ManagingServices />} />
+              <Route path='/AddServices' element={<AdminAuth><AddServices /> </AdminAuth>} />
+              <Route path='/UpdateService/:id' element={<AdminAuth><UpdateService /></AdminAuth>} />
+              <Route path='/Roadmap' element={<Roadmap />} />
+              <Route path='/Contact2' element={<Contact2 />} />
+              <Route path='/view/:id' element={<ViewService />} />
+              <Route path='/ForgetPassword' element={<ForgetPassword />} />
+              
+              <Route path='/UserProfile' element={<UserAuth><UserProfile /></UserAuth>}/>
+              {/* <Route path='/Chat' element={<Chat />} /> */}
+              <Route path='/Admin' element={<AdminAuth> <Main /> </AdminAuth>} > 
+                <Route path='Dashboard' element={ <Dashboard />} />
+                <Route path='Pmanage' element={<Pmanage />} />
+                <Route path='Lmanage' element={<Lmanage />} />
+                <Route path='Mmanage' element={<Mmanage />} />
+                <Route path='AdminProfile' element={<AdminProfile />} />
+                <Route path='Header' element={<Header />} />
+                <Route path='Base' element={<Base />} />
+                <Route path='Umanage' element={<Umanage />} />
+                <Route path='EnquiryData' element={<EnquiryData />} />
+                
+                {/* <Route path='Sidebar' element={<Sidebar />} /> */}
+              </Route>
+
+              {/* <Route path='/ExpertChat' element={<ExpertChat />} /> 
+              <Route path='/UserChat' element={<UserChat />} />  */}
+
+            </Routes>
+            {/* <Chat /> */}
+          </AppProvider>
         </BrowserRouter>
       </SnackbarProvider>
     </div>
