bd7728323a7395935ba141294b05a75fa5051da4
Areeba Shakeel <shakeelareeba02@gmail.com>
2024-06-07 13:15:28 +0530

    ..

--- frontend/src/component/Login.jsx
+++ frontend/src/component/Login.jsx
@@ -1,100 +1,147 @@
 import React from 'react'
 import { useFormik } from 'formik'
+import { Link, useNavigate } from 'react-router-dom'
+import Swal from 'sweetalert2'
 import * as Yup from 'yup'
+import useAppContext from '../../AppContext'
 
+const LoginSchema = Yup.object().shape({
+    password: Yup.string().min(4, 'Too short!').max(20, 'Too Long!'),
+    email: Yup.string().email('Invalid email').required('Required')
+});
 
 const Login = () => {
-  return (
-    <div>
-      <>
-  {/* Section: Design Block */}
-  <section className=" text-center text-lg-start">
-    <style
-      dangerouslySetInnerHTML={{
-        __html:
-          "\n    .rounded-t-5 {\n      border-top-left-radius: 0.5rem;\n      border-top-right-radius: 0.5rem;\n    }\n\n    @media (min-width: 992px) {\n      .rounded-tr-lg-0 {\n        border-top-right-radius: 0;\n      }\n\n      .rounded-bl-lg-5 {\n        border-bottom-left-radius: 0.5rem;\n      }\n    }\n  "
-      }}
-    />
-    <div className="card mb-3">
-      <div className="row g-0 d-flex align-items-center">
-        <div className="col-lg-4 d-none d-lg-flex">
-          <img
-            src="https://mdbootstrap.com/img/new/ecommerce/vertical/004.jpg"
-            alt="Trendy Pants and Shoes"
-            className="w-100 rounded-t-5 rounded-tr-lg-0 rounded-bl-lg-5"
-          />
-        </div>
-        <div className="col-lg-8">
-          <div className="card-body py-5 px-md-5">
-            <form>
-              {/* Email input */}
-              <div data-mdb-input-init="" className="form-outline mb-4">
-                <input
-                  type="email"
-                  id="form2Example1"
-                  className="form-control"
-                />
-                <label className="form-label" htmlFor="form2Example1">
-                  Email address
-                </label>
-              </div>
-              {/* Password input */}
-              <div data-mdb-input-init="" className="form-outline mb-4">
-                <input
-                  type="password"
-                  id="form2Example2"
-                  className="form-control"
-                />
-                <label className="form-label" htmlFor="form2Example2">
-                  Password
-                </label>
-              </div>
-              {/* 2 column grid layout for inline styling */}
-              <div className="row mb-4">
-                <div className="col d-flex justify-content-center">
-                  {/* Checkbox */}
-                  <div className="form-check">
-                    <input
-                      className="form-check-input"
-                      type="checkbox"
-                      defaultValue=""
-                      id="form2Example31"
-                      defaultChecked=""
-                    />
-                    <label
-                      className="form-check-label"
-                      htmlFor="form2Example31"
-                    >
-                      {" "}
-                      Remember me{" "}
-                    </label>
-                  </div>
-                </div>
-                <div className="col">
-                  {/* Simple link */}
-                  <a href="#!">Forgot password?</a>
-                </div>
-              </div>
-              {/* Submit button */}
-              <button
-                type="button"
-                data-mdb-button-init=""
-                data-mdb-ripple-init=""
-                className="btn btn-primary btn-block mb-4"
-              >
-                Sign in
-              </button>
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
-  {/* Section: Design Block */}
-</>
+    const { setLoggedin } = useAppContext();
 
-    </div>
-  )
+    const navigate = useNavigate();
+    const loginForm = useFormik({
+        initialValues: {
+            email: '',
+            password: '',
+        },
+        onSubmit: async (values, action) => {
+            console.log(values);
+
+            const res = await fetch('http://localhost:3000/users1/authenticate', {
+                method: 'POST',
+                body: JSON.stringify(values),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+            console.log(res.status);
+            action.resetForm();
+
+            if (res.status === 200) {
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Login Successful'
+                })
+                setLoggedin(true);
+
+                const data = await res.json();
+                sessionStorage.setItem('isloggedin', true);
+                if (data.role === 'admin') {
+                    sessionStorage.setItem('admin', JSON.stringify(data));
+                    navigate('/admin/base');
+                } else {
+                    sessionStorage.setItem('user', JSON.stringify(data));
+                    navigate('/');
+                }
+            } else if (res.status === 400
+            ) {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Login Failed',
+                    text: 'Invalid email or password'
+
+                })
+            }
+        },
+        // step6: validation of LoginSchema
+        validationSchema: LoginSchema
+    });
+    return (
+        <section className="vh-100">
+            <div className="container-fluid h-custom">
+                <div className="row d-flex justify-content-center align-items-center h-100">
+                    <div className="col-md-9 col-lg-6 col-xl-5">
+                        <img
+                            src="https://i.pinimg.com/736x/6b/1b/22/6b1b22573f9f3d4bba11a9fa5cb45652.jpg"
+                            className="img-fluid"
+                            alt="Sample image"
+                        />
+                    </div>
+                    <div className="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+                        <form onSubmit={loginForm.handleSubmit}>
+
+                            {/* Email input */}
+                            <div className="form-outline mb-4">
+                                <input
+                                    type="email"
+                                    className="form-control form-control-lg"
+                                    placeholder="Enter a valid email address"
+                                    id="email"
+                                    onChange={loginForm.handleChange}
+                                    value={loginForm.values.email}
+                                />
+                                <label className="form-label" htmlFor="form3Example3">
+                                    Email address
+                                </label>
+                                <span style={{ color: 'red', fontsize: '10', marginLeft: 10 }}>{loginForm.touched.name && loginForm.errors.name}</span>
+                            </div>
+                            {/* Password input */}
+                            <div className="form-outline mb-3">
+                                <input
+                                    type="password"
+                                    className="form-control form-control-lg"
+                                    placeholder="Enter password"
+                                    id="password"
+                                    onChange={loginForm.handleChange}
+                                    value={loginForm.values.password}
+                                />
+                                <label className="form-label" htmlFor="form3Example4">
+                                    Password
+                                </label>
+                            </div>
+                            <div className="d-flex justify-content-between align-items-center">
+                                {/* Checkbox */}
+                                <div className="form-check mb-0">
+                                    <input
+                                        className="form-check-input me-2"
+                                        type="checkbox"
+                                        defaultValue=""
+                                        id="form2Example3"
+                                    />
+                                    <label className="form-check-label" htmlFor="form2Example3">
+                                        Remember me
+                                    </label>
+                                </div>
+                                <Link to="/ForgetPassword" className="text-body">
+                                    Forgot password?
+                                </Link>
+                            </div>
+                            <div className="text-center text-lg-start mt-4 pt-2">
+                                <button
+                                    type="submit"
+                                    className="btn btn-primary btn-lg"
+                                    style={{ paddingLeft: "2.5rem", paddingRight: "2.5rem" }}
+                                >
+                                    Login
+                                </button>
+                                <p className="small fw-bold mt-2 pt-1 mb-0">
+                                    Don't have an account?{" "}
+                                    <Link to="/Signup" className="link-danger">
+                                        Register
+                                    </Link>
+                                </p>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    )
 }
-
 export default Login
